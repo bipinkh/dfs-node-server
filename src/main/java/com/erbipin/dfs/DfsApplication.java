4344685b83112ce04f9cb50b132adf5e117265d2
bipinkh <bipinkh91@gmail.com>
2018-06-26 00:31:29 +0545

    initialized project

---
+++ src/main/java/com/erbipin/dfs/DfsApplication.java
@@ -0,0 +1,12 @@
+package com.erbipin.dfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DfsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DfsApplication.class, args);
+	}
+}
